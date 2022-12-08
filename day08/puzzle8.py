6e033d9f75b0a8d7aaff103bf356d11cd91cd5fe
Joris Bastiaans <joris.bastiaans@transavia.com>
2022-12-08 13:48:36 +0100

    Cosmetic improvements on day 8 solution

--- day08/puzzle8.py
+++ day08/puzzle8.py
@@ -1,16 +1,18 @@
 from helpers import loadMap, _width, _height, _left, _right, _top, _bottom
 
+
 def isVisibleFromOutside(x: int, y:int, trees:list[str]) -> bool:
-    if x == 0 or y == 0 or x >= _width(trees)-1 or y >= _height(trees)-1:
+    if (x == 0) or (y == 0) or (x >= _width(trees) - 1) or (y >= _height(trees) - 1):
         return True
     else:
         tree_height = int(trees[y][x])
         return (
-            tree_height > max(_left(x,y,trees)) or
-            tree_height > max(_right(x,y,trees)) or 
-            tree_height > max(_top(x,y,trees)) or
-            tree_height > max(_bottom(x,y,trees))
+            tree_height > max(_left(x, y, trees)) or
+            tree_height > max(_right(x, y, trees)) or 
+            tree_height > max(_top(x, y, trees)) or
+            tree_height > max(_bottom(x, y, trees))
         )
+
 
 def freeSightDistance(tree_height: int, neighbours:list[str]) -> int:
     visibility = 0
@@ -19,6 +21,7 @@ def freeSightDistance(tree_height: int, neighbours:list[str]) -> int:
         if neighbour >= tree_height:
             break
     return visibility
+
 
 def scenicScore(x: int, y:int, trees:list[str]) -> int:
     h = int(trees[y][x])
@@ -29,6 +32,10 @@ def scenicScore(x: int, y:int, trees:list[str]) -> int:
         freeSightDistance(h, _bottom(x, y, trees))
     )
 
+
 trees, width, height = loadMap()
-print(f"Visible trees: {sum([1  for y in range(height) for x in range(width) if isVisibleFromOutside(x, y, trees) ])}.")
-print(f"Highest scenic score: {max([scenicScore(x, y, trees) for x in range(width) for y in range(height)])}.")
+
+print(f"""
+    Visible trees: {sum([1  for y in range(height) for x in range(width) if isVisibleFromOutside(x, y, trees) ])}.
+    Highest scenic score: {max([scenicScore(x, y, trees) for x in range(width) for y in range(height)])}.
+""")
