cf3882597608f011b8413d10d136f15bfd78ae75
Joris Bastiaans <joris.bastiaans@transavia.com>
2022-12-10 22:29:10 +0100

    cleanup

--- day10/puzzle10.py
+++ day10/puzzle10.py
@@ -10,9 +10,6 @@ with open('input.txt', 'r') as f:
 def x_value_at(cycle: int) -> int:
     return 1 + sum([ops[i][1] for i in range(len(ops)) if sum([op[0] for op in ops[:i + 1]]) <= (cycle - 1)])
 
-def x_value_after(cycle: int) -> int:
-    return x_value_at(cycle + 1)
-
 def signal_strength_at(cycle: int) -> int:
     return cycle * x_value_at(cycle)
 
@@ -22,7 +19,7 @@ def answer1() -> int:
 def answer2() -> str:
     crt = ""
     for cycle in range(1, 240 + 1):
-        x = x_value_after(cycle - 1)
+        x = x_value_at(cycle)
         crt += "#" if (cycle - 1) % 40 in (x - 1, x, x + 1) else "."
     return "\n".join(wrap(crt, 40))
 
@@ -35,19 +32,17 @@ print(f"""
     Yay!
 """)
 
-"""
-Right after completion:
-- Felt smart about loading/coercing the input data
-  - string-replacing the zero cost of noop into the input, before translating using a dictionary.
-- Made a more functional approach to determining the signal strength and value of x
-  - able to do it on one line
-- Not really happy that I kept `ops` a global variable, but 'hey, it works!'
-Right after seeing other people's solutions:
-- This time, procedural might have actually been easier :-D (as proven by part 2)
-- dictionaries
-Learned:
-- nested and conditional list comprehension (line 7 and 10)
-- conditional value assignment (var = 'someval' if (condition) else 'somethingelse')
-- textwrap module
-- joining a list of string into an uber-string (line 27)
-"""
+# Right after completion:
+# - Felt smart about loading/coercing the input data
+#   - string-replacing the zero cost of noop into the input, before translating using a dictionary.
+# - Made a more functional approach to determining the signal strength and value of x
+#   - able to do it on one line
+# - Not really happy that I kept `ops` a global variable, but 'hey, it works!'
+# Right after seeing other people's solutions:
+# - This time, procedural might have actually been easier :-D (as proven by part 2)
+# - dictionaries
+# Learned:
+# - nested and conditional list comprehension (line 7 and 10)
+# - conditional value assignment (var = 'someval' if (condition) else 'somethingelse')
+# - textwrap module
+# - joining a list of string into an uber-string (line 27)
